bf6a28658b77cf67eb7a695192da1d1108f229f2
michael wilson <untenyan@fullsail.edu>
2012-01-09 17:50:27 -0700

    added first files to the repo

---
+++ Wilson_Michael_Deliverable1.js
@@ -0,0 +1,55 @@
+alert("JavaScript works!");
+// Author: Michael Wilson
+// 01-04-2012
+// Deliverable 1
+// Heros VS Villans
+
+//standard variable
+var E = true,
+	F = false,
+	Trip,
+	Call = "Call Friends",
+	Well
+;
+
+//Heros
+var A = "Happy Family",
+	B = "Travel to seattle in 2 days",
+	hero1,
+	hero2
+;
+
+//Villan
+var C = "Unhappy Family",
+	D = "Travel to seattle in 1 day",
+	villan1,
+	villan2
+;
+
+(Trip = Call)
+
+console.log(Trip)
+
+if (Well == E)
+	{
+	hero1 = A
+	}
+
+if (hero1 == A)
+	{
+	hero2 = B
+	}
+
+console.log(hero1, "gets to", hero2)
+		 
+if (Well == F)
+	{
+	villan1 = C
+	}
+
+if (villan1 == C)
+	{
+	villan2 = D
+	}
+	
+console.log(villan1, "has to", villan2)
