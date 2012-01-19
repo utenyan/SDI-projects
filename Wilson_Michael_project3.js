2e8873787c7608fde4bcc9b85d921ae0253617fc
michael wilson <untenyan@fullsail.edu>
2012-01-19 15:21:18 -0700

    fourth commit project 3

--- Wilson_Michael_project3.js
+++ Wilson_Michael_project3.js
@@ -15,7 +15,7 @@ if (Trip == E)
 	console.log(Call);
 	}else{
 	console.log("We are not going to washington");
-	}
+}
 
 //Given: (Well ^ Ill) v Ill
 if ((E && F) || G)
@@ -23,12 +23,51 @@ if ((E && F) || G)
 	console.log("Are friends are Well");
 	}else{
 	console.log("Are friends are Ill");
-	}
+}
 
 if (Trip == E)
 	{
 	console.log("We need to pack our suitcase");
+}
+	
+var Mom = {
+	name: "Jamie",
+	age: "26",
+	role: "Caretaker",
+	sayPacked: function () {
+		console.log ("All the suitcases are packed");
 	}
+};
+
+for (var key in Mom) {
+	console.log("Mom - " + key + ": " + Mom[key]);
+};
+
+var Dad = {
+	name: "Mike",
+	age: "30",
+	role: "Student",
+	sayLoaded: function () {
+		console.log ("I have all the suitcases in the van");
+	}
+};
+
+for (var key1 in Dad) {
+	console.log("Dad - " + key1 + ": " + Dad[key1]);
+};
+
+var Daughter = {
+	name: "Shayla",
+	age: "4",
+	role: "Just here to be cute",
+	sayBoard: function () {
+		console.log ("I am board, where is my toy!!");
+	}
+};
+
+for (var key2 in Daughter) {
+	console.log("Daughter - " + key2 + ": " + Daughter[key2]);
+};
 	
 var suitcaseNames = [ "Moms", "Dads", "Daughters" ],
 	minutesToPack = [ 25, 15, 45 ];
@@ -57,14 +96,14 @@ var heros = ["friends are well",
 
 for (var i=0; i < 3; i++) {
 	console.log(heros[i]);
-	}
+}
 
 //first days trip
 var hours = 7
 while (hours > 0) {
 	console.log(hours + "hours left to friends house");
 	hours--;
-	}
+}
 	
 var hours = 18;
 var firstday = 7;
@@ -72,11 +111,11 @@ var firstday = 7;
 var getsecondday = function (hours, firstday) {
 	var secondday = hours - firstday;
 	return secondday;
-	};
+};
 		
 var reportsecondday = function(secondday, message){
 	console.log("The time left to drive for tomorrow is "  + secondday +  " hours");
-	};
+};
 
 reportsecondday(11);
 	
@@ -85,12 +124,12 @@ var hours = 11
 while (hours > 0) {
 	console.log(hours + "hours left to seattle from friends house");
 	hours--;
-	}
+}
 	
 if (friends == Ill)
 	{
 	console.log("We have to travel streight to seattle in 18hours");
-	}
+}
 	
 var villians = ["friends are ill",
 	"family is unhappy",
@@ -99,10 +138,10 @@ var villians = ["friends are ill",
 
 for (var i=0; i < 3; i++) {
 	console.log(villians[i]);
-	}
+}
 	
 var hours = 18
 while (hours > 0) {
 	console.log(hours + "hours left to reach seattle");
 	hours--;
-	}
+}
